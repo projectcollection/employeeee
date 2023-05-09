16fe19ae353d12acd545bf1f4167e4dde1453656
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-05-09 10:50:31 -0400

    refactor to switch statement

--- src/server.ts
+++ src/server.ts
@@ -11,114 +11,120 @@ const server = http.createServer((req, res) => {
         const [path, queryString] = req.url?.split('?');
 
         if (path === '/employees') {
-            if (req.method == "GET") {
-                if (!queryString) {
-                    const allEmployees = executionMap['all']();
+            switch (req.method) {
+                case 'GET': {
+                    if (!queryString) {
+                        const allEmployees = executionMap['all']();
 
-                    if (allEmployees) {
-                        const jsonEmployees = allEmployees.split('\n').map((entry) => {
-                            return strToJSON(entry);
-                        });
+                        if (allEmployees) {
+                            const jsonEmployees = allEmployees.split('\n').map((entry) => {
+                                return strToJSON(entry);
+                            });
+
+                            res.setHeader('content-type', 'application/json');
+                            res.statusCode = 200;
+                            res.end(JSON.stringify(jsonEmployees));
+                        }
+                    } else {
+
+                        const [key, val] = queryString.split('=');
+
+                        if (key === 'id') {
+                            try {
+                                const employee = executionMap['id'](val);
+
+                                if (employee) {
+                                    res.setHeader('content-type', 'application/json');
+                                    res.statusCode = 200;
+                                    res.end(JSON.stringify(strToJSON(employee.toString())));
+                                }
+                            } catch (err) {
+                                res.statusCode = 404;
+                                res.end('employee not found');
+                            }
+                        } else if (key === 'name') {
+                            try {
+                                const matches = executionMap['name'](val);
+
+                                if (matches) {
+                                    const jsonEmployees = matches.split('\n').map((entry) => {
+                                        return strToJSON(entry);
+                                    }).filter((entry) => {
+                                        return entry.name === val;
+                                    });
+
+                                    res.setHeader('content-type', 'application/json');
+                                    res.statusCode = 200;
+                                    res.end(JSON.stringify(jsonEmployees));
+                                }
+                            } catch (err) {
+                                res.statusCode = 404;
+                                res.end('employee not found');
+                            }
+                        } else if (key === 'email') {
+                            try {
+                                const matches = executionMap['email'](decodeURIComponent(val));
+
+                                if (matches) {
+                                    const jsonEmployees = matches.split('\n').map((entry) => {
+                                        return strToJSON(entry);
+                                    })
+
+                                    res.setHeader('content-type', 'application/json');
+                                    res.statusCode = 200;
+                                    res.end(JSON.stringify(jsonEmployees));
+                                }
+                            } catch (err) {
+                                res.statusCode = 404;
+                                res.end('employee not found');
+                            }
+                        }
+                    }
+                    break;
+                }
+                case 'POST': {
+                    let chunks: Uint8Array[] = [];
+
+                    req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                    });
+
+                    req.on('end', () => {
+                        const data = Buffer.concat(chunks);
+                        //todo: need validation
+                        const newEmployee = new Employee(JSON.parse(data.toString()))
 
                         res.setHeader('content-type', 'application/json');
                         res.statusCode = 200;
-                        res.end(JSON.stringify(jsonEmployees));
-                    }
-                } else {
-
-                    const [key, val] = queryString.split('=');
-
-                    if (key === 'id') {
-                        try {
-                            const employee = executionMap['id'](val);
-
-                            if (employee) {
-                                res.setHeader('content-type', 'application/json');
-                                res.statusCode = 200;
-                                res.end(JSON.stringify(strToJSON(employee.toString())));
-                            }
-                        } catch (err) {
-                            res.statusCode = 404;
-                            res.end('employee not found');
-                        }
-                    } else if (key === 'name') {
-                        try {
-                            const matches = executionMap['name'](val);
-
-                            if (matches) {
-                                const jsonEmployees = matches.split('\n').map((entry) => {
-                                    return strToJSON(entry);
-                                }).filter((entry) => {
-                                    return entry.name === val;
-                                });
-
-                                res.setHeader('content-type', 'application/json');
-                                res.statusCode = 200;
-                                res.end(JSON.stringify(jsonEmployees));
-                            }
-                        } catch (err) {
-                            res.statusCode = 404;
-                            res.end('employee not found');
-                        }
-                    } else if (key === 'email') {
-                        try {
-                            const matches = executionMap['email'](decodeURIComponent(val));
-
-                            if (matches) {
-                                const jsonEmployees = matches.split('\n').map((entry) => {
-                                    return strToJSON(entry);
-                                })
-
-                                res.setHeader('content-type', 'application/json');
-                                res.statusCode = 200;
-                                res.end(JSON.stringify(jsonEmployees));
-                            }
-                        } catch (err) {
-                            res.statusCode = 404;
-                            res.end('employee not found');
-                        }
-                    }
+                        res.end(JSON.stringify(strToJSON(newEmployee.toString())));
+                    });
+                    break;
                 }
-            } else if (req.method == 'POST') {
-                let chunks: Uint8Array[] = [];
+                case 'DELETE': {
+                    let chunks: Uint8Array[] = [];
 
-                req.on('data', (chunk) => {
-                    chunks.push(chunk);
-                });
+                    req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                    });
 
-                req.on('end', () => {
-                    const data = Buffer.concat(chunks);
-                    //todo: need validation
-                    const newEmployee = new Employee(JSON.parse(data.toString()))
+                    req.on('end', () => {
+                        const data = JSON.parse(Buffer.concat(chunks).toString());
 
-                    res.setHeader('content-type', 'application/json');
-                    res.statusCode = 200;
-                    res.end(JSON.stringify(strToJSON(newEmployee.toString())));
-                })
-            } else if (req.method == 'DELETE') {
-                let chunks: Uint8Array[] = [];
+                        try {
+                            if (data.id) {
+                                executionMap['del'](`${data.id}`);
+                                res.setHeader('content-type', 'application/json');
+                                res.statusCode = 200;
+                                res.end(JSON.stringify({ message: "deleted" }));
+                            }
 
-                req.on('data', (chunk) => {
-                    chunks.push(chunk);
-                });
-
-                req.on('end', () => {
-                    const data = JSON.parse(Buffer.concat(chunks).toString());
-
-                    try {
-                        if (data.id) {
-                            executionMap['del'](`${data.id}`);
+                        } catch (err) {
                             res.setHeader('content-type', 'application/json');
-                            res.statusCode = 200;
-                            res.end(JSON.stringify({ message: "deleted" }));
+                            res.statusCode = 404;
+                            res.end(JSON.stringify({ message: "not found" }));
                         }
-
-                    } catch (err) {
-                        res.setHeader('content-type', 'application/json');
-                        res.statusCode = 404;
-                        res.end(JSON.stringify({ message: "not found" }));
-                    }
-                })
+                    });
+                }
             }
         }
     }
