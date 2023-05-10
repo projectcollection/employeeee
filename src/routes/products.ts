b3b72aa97b14be2c791ff859a07121b0d755d1e5
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-05-10 11:17:27 -0400

    add zod validation

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,35 +1,44 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import z from 'zod';
 
-//todo: use zod for types/validation
-export type Image = {
-    thumbnail: string,
-    original: string
-};
+export const ImageSchema = z.object({
+    thumbnail: z.string(),
+    original: z.string()
+});
 
-export type Attribute = {
-    id: number,
-    name: string,
-    slug: string
-}
+export type Image = z.infer<typeof ImageSchema>;
 
-export type Variations = {
-    id: number,
-    value: string,
-    attribute: Attribute
-};
 
-export type Product = {
-    id: string,
-    name: string,
-    description: string,
-    slug: string,
-    image: Image,
-    gallery: Image[],
-    price: number,
-    sale_price: number,
-    variations: Variations[]
-}
+export const AttributeSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    slug: z.string()
+});
+
+export type Attribute = z.infer<typeof AttributeSchema>;
+
+export const VariationSchema = z.object({
+    id: z.number(),
+    value: z.string(),
+    attribute: AttributeSchema
+});
+
+export type Variation = z.infer<typeof VariationSchema>;
+
+export const ProductSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    slug: z.string(),
+    image: ImageSchema,
+    gallery: z.array(ImageSchema),
+    price: z.number(),
+    sale_price: z.number(),
+    variations: z.array(VariationSchema)
+});
+
+export type Product = z.infer<typeof ProductSchema>;
 
 const router = express.Router();
 
@@ -43,15 +52,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    //todo: need validation
-    let newProduct = req.body;
-    newProduct.id = uuidv4();
+    try {
+        let newProduct = ProductSchema.partial({ id: true }).parse(req.body);
+        newProduct.id = uuidv4();
 
-    tempProducts.push(newProduct as Product);
+        tempProducts.push(newProduct as Product);
 
-    res.setHeader('content-type', 'application/json');
-    res.statusCode = 200;
-    res.end(JSON.stringify({ message: "added", product: newProduct }));
+        res.setHeader('content-type', 'application/json');
+        res.statusCode = 200;
+        res.end(JSON.stringify({ message: "added", product: newProduct }));
+    } catch (error) {
+
+        res.setHeader('content-type', 'application/json');
+        res.statusCode = 400;
+        res.end(JSON.stringify({ message: "error", error }));
+    }
 });
 
 router.delete('/', (req, res) => {
